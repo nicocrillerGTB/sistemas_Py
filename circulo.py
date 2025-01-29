5366465a830bd00ec2b2e0dd0fc28d9241faaca8
nicocriller <nicolasmejia@colegioguanenta.edu.co>
2025-01-29 09:08:38 -0500

    Delete circulo.py

--- circulo.py
+++
@@ -1,8 +0,0 @@
-import math
-
-radio = float(input("Ingrese el radio del círculo: "))
-
-# Calculamos el área utilizando la constante pi de la biblioteca math
-area = math.pi * radio**2
-
-print("El área del círculo es:", area)
