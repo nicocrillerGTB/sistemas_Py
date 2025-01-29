1fde7b966216cea9c18ce9c8b3c8ff288cd007c5
nicocriller <nicolasmejia@colegioguanenta.edu.co>
2025-01-29 09:07:03 -0500

    Delete suma 3 dig.py

--- suma 3 dig.py
+++
@@ -1,8 +0,0 @@
-import time
-
-print(f"suma de 2 digitos\npara empezar, necesitamos los numeros")
-
-n1 = int(input("numero 1\n="))
-n2 = int(input("numero 2\n="))
-
-print(f"el resultado de la suma es { n1 + n2 }")
