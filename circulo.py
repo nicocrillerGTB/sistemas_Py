01074db6caa59bfecb69babf9091663aba207502
nicocriller <nicolasmejia@colegioguanenta.edu.co>
2025-01-29 06:11:46 -0500

    Add files via upload

---
+++ circulo.py
@@ -0,0 +1,8 @@
+import math
+
+radio = float(input("Ingrese el radio del círculo: "))
+
+# Calculamos el área utilizando la constante pi de la biblioteca math
+area = math.pi * radio**2
+
+print("El área del círculo es:", area)
---
+++ triangulo.py
@@ -0,0 +1,7 @@
+print("calcular base por altura de un triangulo")
+base = int(input("cual es la base?"))
+altura = int(input("cual es la alura?"))
+
+resultado = int((base * altura) / 2)
+
+print("el area del triangulo es", resultado)
